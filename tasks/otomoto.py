fcc369880b67331f7c59e653fd476551cbac53af
manitou <sibuk.harabudjasim@gmail.com>
2017-12-31 02:01:30 +0200

    Otomoto new links precise check.

--- tasks/otomoto.py
+++ tasks/otomoto.py
@@ -8,11 +8,17 @@ from utils.common import Hosts
 class OtomotoParser(BaseParserTask):
     type = Hosts.OTOMOTO
 
+    def __init__(self, task, from_user, call_interval):
+        super().__init__(task, from_user, call_interval)
+        self.seen_urls = set()
+
     def parse_ads_list(self, document):
         ads = document.xpath('//article[contains(@class, "offer-item")]')
-        for ad in ads:
-            url = ad.xpath('.//a[@class="offer-title__link"]/@href')[0]
+        urls = set([ad.xpath('.//a[@class="offer-title__link"]/@href')[0] for ad in ads])
+        new_urls = urls - self.seen_urls
+        for url in new_urls:
             asyncio.ensure_future(self.parse_ad(url))
+        self.seen_urls = urls
 
 
 __author__ = 'manitou'
